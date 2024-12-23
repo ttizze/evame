cadd2425559ccd4d4d88c814a43af2fd60c39e8e
tomolld <tomoki2757@gmail.com>
2024-12-23 10:40:47 +0900

    LocaleLink と NavLocaleLink コンポーネントで useMatches を useParams に置き換え、ルートデータの取得方法を簡素化

--- web/app/components/LocaleLink.tsx
+++ web/app/components/LocaleLink.tsx
@@ -1,11 +1,6 @@
 // LocaleLink.tsx
-import { Link, useMatches } from "@remix-run/react";
+import { Link, useParams } from "@remix-run/react";
 
-function useRootData() {
-	const matches = useMatches();
-	const rootMatch = matches.find((match) => match.id === "root");
-	return rootMatch?.data as { locale: string } | undefined;
-}
 
 export function LocaleLink({
 	to,
@@ -16,8 +11,7 @@ export function LocaleLink({
 	children: React.ReactNode;
 	className?: string;
 }) {
-	const rootData = useRootData();
-	const locale = rootData?.locale ?? "en";
+  const { locale } = useParams();
 
 	const path = `/${locale}${to}`;
 
--- web/app/components/NavLocaleLink.tsx
+++ web/app/components/NavLocaleLink.tsx
@@ -1,10 +1,5 @@
-import { NavLink, type NavLinkProps, useMatches } from "@remix-run/react";
+import { NavLink, type NavLinkProps, useParams } from "@remix-run/react";
 
-function useRootData() {
-	const matches = useMatches();
-	const rootMatch = matches.find((match) => match.id === "root");
-	return rootMatch?.data as { locale?: string } | undefined;
-}
 
 // NavLinkProps を継承
 type NavLocaleLinkProps = Omit<NavLinkProps, "to"> & {
@@ -17,17 +12,14 @@ export function NavLocaleLink({
 	children,
 	...rest
 }: NavLocaleLinkProps) {
-	const rootData = useRootData();
-	const locale = rootData?.locale ?? "en";
+  const { locale } = useParams();
 
-	// 先頭のスラッシュを削除したうえで "/:locale" を付与
 	const normalized = to.startsWith("/") ? to.slice(1) : to;
 	const path = `/${locale}/${normalized}`;
 
 	return (
 		<NavLink
 			to={path}
-			// className が「文字列」or「コールバック」どちらでもOK
 			className={className}
 			{...rest}
 		>
