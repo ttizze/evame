d19d0f20364d09a9b41f6c50c454033154db818f
tomolld <tomoki2757@gmail.com>
2025-02-12 22:38:00 +0900

    middlewareのマッチャー設定を更新し、静的ファイルや特定のリソースを除外しました。

--- next/src/middleware.ts
+++ next/src/middleware.ts
@@ -6,5 +6,7 @@ const handleI18nRouting = createMiddleware(routing);
 export default auth(handleI18nRouting);
 
 export const config = {
-	matcher: ["/((?!api|_next|.*..*).*)"],
+	matcher: [
+		"/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt).*)",
+	],
 };
