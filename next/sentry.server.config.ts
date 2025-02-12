a3e2da092e45accf50d715d7a78663c754b14bd8
tomolld <tomoki2757@gmail.com>
2025-02-12 22:04:27 +0900

    Sentryの初期化を環境に応じて条件付きに変更し、プロダクション環境でのみ設定を適用するようにしました。

--- next/sentry.server.config.ts
+++ next/sentry.server.config.ts
@@ -4,12 +4,14 @@
 
 import * as Sentry from "@sentry/nextjs";
 
-Sentry.init({
-	dsn: "https://0cda4c09dab97bb05116614428effb0c@o4507906314207232.ingest.us.sentry.io/4508805630263296",
+if (process.env.NODE_ENV === "production") {
+	Sentry.init({
+		dsn: "https://0cda4c09dab97bb05116614428effb0c@o4507906314207232.ingest.us.sentry.io/4508805630263296",
 
 	// Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
 	tracesSampleRate: 1,
 
-	// Setting this option to true will print useful information to the console while you're setting up Sentry.
-	debug: false,
-});
+		// Setting this option to true will print useful information to the console while you're setting up Sentry.
+		debug: false,
+	});
+}
