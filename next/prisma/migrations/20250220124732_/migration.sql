572bc556974c72e277a708c5b72cdbaf52b6f0b6
tomolld <tomoki2757@gmail.com>
2025-02-20 22:52:32 +0900

    page_ai_translation_infoテーブルからai_translation_progressカラムを削除

--- next/prisma/migrations/20250220124732_/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "page_ai_translation_info" ADD COLUMN     "ai_translation_progress" INTEGER NOT NULL DEFAULT 0;
--- next/prisma/migrations/20250220134830_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to drop the column `ai_translation_progress` on the `page_ai_translation_info` table. All the data in the column will be lost.
-
-*/
--- AlterTable
-ALTER TABLE "page_ai_translation_info" DROP COLUMN "ai_translation_progress";
