18535e3629c1e4b97287446a1ac1b39aee33e956
tomolld <tomoki2757@gmail.com>
2025-03-18 18:26:04 +0900

    ページコメント削除機能のコードを整形し、削除時にコンテンツを「deleted」に変更する処理を明確化しました。

--- next/src/app/[locale]/(common-layout)/user/[handle]/page/[slug]/_components/comment/_components/page-comment-list/page-comment-item/_db/mutations.server.ts
+++ next/src/app/[locale]/(common-layout)/user/[handle]/page/[slug]/_components/comment/_components/page-comment-list/page-comment-item/_db/mutations.server.ts
@@ -1,10 +1,10 @@
 import { prisma } from "@/lib/prisma";
 
 export async function deletePageComment(pageCommentId: number) {
-  return await prisma.pageComment.update({
-    where: { id: pageCommentId },
-    data: {
-      content: "deleted",
-    },
-  });
+	return await prisma.pageComment.update({
+		where: { id: pageCommentId },
+		data: {
+			content: "deleted",
+		},
+	});
 }
