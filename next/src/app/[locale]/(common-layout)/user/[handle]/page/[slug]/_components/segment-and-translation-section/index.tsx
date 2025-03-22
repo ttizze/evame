b32c84c7afec20099b07abb9cac4f9c391e8954e
tomolld <tomoki2757@gmail.com>
2025-03-22 21:48:31 +0900

    ヘッダーコンポーネントのボタンラベルを「Public」から「Public & Translate」に変更しました

--- next/src/app/[locale]/(common-layout)/user/[handle]/page/[slug]/_components/segment-and-translation-section/index.tsx
+++ next/src/app/[locale]/(common-layout)/user/[handle]/page/[slug]/_components/segment-and-translation-section/index.tsx
@@ -46,18 +46,15 @@ export function SegmentAndTranslationSection({
 	return (
 		<span className="flex flex-col">
 			{showOriginal && (
-				<span>
-					<span
-						className={`inline-block ${
-							segmentWithTranslations.segmentTranslationsWithVotes.length ===
-								0 || !showTranslation
-								? "text-gray-700 dark:text-gray-200 [&>a]:text-gray-700 dark:[&>a]:text-gray-200 [&>strong]:text-gray-700 dark:[&>strong]:text-gray-200"
-								: "text-gray-300 dark:text-gray-600 [&>a]:text-gray-300 dark:[&>a]:text-gray-600 [&>strong]:text-gray-300 dark:[&>strong]:text-gray-600"
-						} ${segmentTextClassName}`}
-					>
-						{showLockIcon && <Lock className="h-6 w-6 mr-1 inline" />}
-						{elements}
-					</span>
+				<span
+					className={`inline-block ${
+						segmentWithTranslations.segmentTranslationsWithVotes.length === 0 ||
+						!showTranslation
+							? "text-gray-700 dark:text-gray-200 [&>a]:text-gray-700 dark:[&>a]:text-gray-200 [&>strong]:text-gray-700 dark:[&>strong]:text-gray-200"
+							: "text-gray-300 dark:text-gray-600 [&>a]:text-gray-300 dark:[&>a]:text-gray-600 [&>strong]:text-gray-300 dark:[&>strong]:text-gray-600"
+					} ${segmentTextClassName}`}
+				>
+					{showLockIcon && <Lock className="h-6 w-6 mr-1 inline" />}
 					{isOwner && slug && (
 						<div className="ml-2">
 							<Link href={`/user/${currentHandle}/page/${slug}/edit`}>
@@ -65,6 +62,7 @@ export function SegmentAndTranslationSection({
 							</Link>
 						</div>
 					)}
+					{elements}
 				</span>
 			)}
 			{showTranslation &&
