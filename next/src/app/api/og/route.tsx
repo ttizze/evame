b245fbbd7ec37da0d86c912a96db77b771ae0e72
tomolld <tomoki2757@gmail.com>
2025-04-22 10:18:44 +0900

    ユーザーのロケールを追加し、ヘッダーのスタイルを改善しました。また、OGルートの再検証設定を動的に変更しました。

--- next/src/app/api/og/route.tsx
+++ next/src/app/api/og/route.tsx
@@ -4,7 +4,7 @@ import { fetchPageContext } from "@/app/[locale]/(common-layout)/user/[handle]/p
 import type { DisplayMode } from "@/app/_context/display-types";
 import { ImageResponse } from "next/og";
 
-export const revalidate = 3600;
+export const dynamic = "force-static";
 export async function GET(req: Request): Promise<Response> {
 	const { searchParams } = new URL(req.url);
 	const interFontSemiBold = await readFile(
